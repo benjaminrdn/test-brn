c40cf00db7b05e71483beb4d6edd1efdfa1b597e
Benjamin <benjamin.raudin@gmail.com>
2025-07-04 14:46:50 +0000

    feat: app init

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('renders Rsbuild heading', () => {
+    render(<App />);
+    expect(screen.getByText(/Rsbuild with React/i)).toBeInTheDocument();
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>On brn-test</p>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
